32dbe653691f5e9ce92a3bbb9e1b82229cf408d2
Alexandr <Smyasnikov03@mail.ru>
2023-03-24 21:17:09 +0300

    Constraint Layout and initialization of several components in the layout with data from the program code

--- app/src/main/java/com/example/mobile_myasnikov_pr2/MainActivity.java
+++ app/src/main/java/com/example/mobile_myasnikov_pr2/MainActivity.java
@@ -1,7 +1,8 @@
 package com.example.mobile_myasnikov_pr2;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,5 +11,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView loadTitle = (TextView)findViewById(R.id.textAct);
+        loadTitle.setText(R.string.Successfully);
+        ImageView loadPicture = (ImageView)findViewById(R.id.kartinka);
+        loadPicture.setImageResource(R.drawable.svgimage);
     }
 }
